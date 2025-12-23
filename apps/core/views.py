227f356017a41b83985cdf78e995434a3588142f
Rogério B. Martins <rogeriodelphi@gmail.com>
2025-12-23 02:14:23 -0300

    Somando os totais de alunos, livros e de empréstimos

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,6 +1,17 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-
+from apps.alunos.models import Aluno
+from apps.livros.models import Livro
+from apps.emprestimos.models import Emprestimo
 
 def index(request):
-    return render(request, 'index.html')
+    total_alunos =  Aluno.objects.all().count()
+    total_livros = Livro.objects.all().count()
+    total_emprestimos = Emprestimo.objects.filter(status='E').count()
+    context = {
+        'total_alunos': total_alunos,
+        'total_livros': total_livros,
+        'total_emprestimos': total_emprestimos,
+
+    }
+    return render(request, 'index.html', context)
