a9246c397b7fafde92300845d25f8575d3680003
Rogério B. Martins <rogeriodelphi@gmail.com>
2025-12-23 00:11:34 -0300

    Adicionando a herança de Template

--- biblioteca/urls.py
+++ biblioteca/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('index/', include("apps.core.urls")),
+    path('', include("apps.core.urls")),
     path('alunos/', include('apps.alunos.urls', namespace='alunos')),
     path('livros/', include('apps.livros.urls', namespace='livros')),
     path('emprestimos/', include('apps.emprestimos.urls', namespace='emprestimos')),
